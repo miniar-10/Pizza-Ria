e262271a4811a8e3a202bb2f986ee2f12eb5c7be
miniar-10 <miniar.lasmar@etudiant-fst.utm.tn>
2025-06-05 23:27:52 +0200

    Added integration tests with In Memory Data

---
+++ IntegrationTests/AuthIntegrationTests.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationTests
+{
+    public class AuthIntegrationTests : IClassFixture<CustomWebApplicationFactory>
+    {
+        private readonly HttpClient _client;
+
+        public AuthIntegrationTests(CustomWebApplicationFactory factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Register_ReturnsSuccess()
+        {
+            var payload = new StringContent(JsonConvert.SerializeObject(new
+            {
+                Name = "test",
+                Password = "123"
+            }), Encoding.UTF8, "application/json");
+
+            var response = await _client.PostAsync("/api/auth/register", payload);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+
+}
---
+++ IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Backend.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace IntegrationTests
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType == typeof(DbContextOptions<AppDbContext>));
+                if (descriptor != null) services.Remove(descriptor);
+
+                services.AddDbContext<AppDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("TestDb");
+                });
+            });
+        }
+    }
+
+}
