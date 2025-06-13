692952dd85be8f4ed5676b1599445d8f9993e872
miniar-10 <miniar.lasmar@etudiant-fst.utm.tn>
2025-06-13 15:10:19 +0200

    debugging..

--- Backend/Services/DatabaseConfigService.cs
+++ Backend/Services/DatabaseConfigService.cs
@@ -1,4 +1,5 @@
 ﻿using Backend.Models;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Backend.Services
 {
@@ -16,12 +17,12 @@ namespace Backend.Services
             var baseString = _config.GetConnectionString("DefaultConnection");
             Console.WriteLine("ENV VAR: " + Environment.GetEnvironmentVariable("DB_PASSWORD"));
 
-            baseString = baseString
+            baseString = baseString?
                 .Replace("{password}", Environment.GetEnvironmentVariable("DB_PASSWORD"));
             Console.WriteLine(baseString);
 
 
-            return baseString;
+            return baseString?? "";
 
 
         }
